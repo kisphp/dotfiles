4fa827ce36a4e020554adb380395d32a6bbbd3bd
Bogdan Rizac <bogdan.rizac@lumas.com>
2017-08-17 17:23:38 +0200

    fixed colors

--- install.sh
+++ install.sh
@@ -1,12 +1,24 @@
 #!/usr/bin/env bash
 
-ERROR="\033[41m" # background red
-GREEN="\033[42m" # background green
-INFO="\033[43m" # yellow text
-BACKGROUND="\033[46m" # background blue
-BLACKTEXT="\033[30m"
-COLOR="\033[39m" # text white
-NC="\033[0m" # reset
+# Use colors, but only if connected to a terminal, and that terminal
+# supports them.
+if [ -t 1 ]; then
+    ERROR="\033[41m" # background red
+    GREEN="\033[42m" # background green
+    INFO="\033[43m" # yellow text
+    BACKGROUND="\033[46m" # background blue
+    BLACKTEXT="\033[30m"
+    COLOR="\033[39m" # text white
+    NC="\033[0m" # reset
+else
+    ERROR="-->"
+    GREEN="-->"
+    INFO="-->"
+    BACKGROUND="-->"
+    BLACKTEXT="-->"
+    COLOR="-->"
+    NC="-->"
+fi
 
 GIT=`which git`
 
