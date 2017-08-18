a5a85843f8fe7b6fc95e065aa2b43d276bfbdcc4
Bogdan Rizac <mariusbogdan83@gmail.com>
2017-08-18 07:44:13 +0200

    changed colors in install script

--- install.sh
+++ install.sh
@@ -1,23 +1,35 @@
 #!/usr/bin/env bash
 
+# Foreground colors
+FG_RED="\033[31m"
+FG_WHITE="\033[97m"
+FG_GREEN="\033[32m"
+FG_BLACK="\033[30m"
+FG_YELLOW="\033[33m"
+FG_BLUE="\033[34m"
+
+# Background colors
+BG_RED="\033[41m"
+BG_WHITE="\033[107m"
+BG_GREEN="\033[42m"
+BG_BLACK="\033[40m"
+BG_YELLOW="\033[43m"
+BG_BLUE="\033[44m"
+
 # Use colors, but only if connected to a terminal, and that terminal
 # supports them.
 if [ -t 1 ]; then
-    ERROR="\033[41m" # background red
-    GREEN="\033[42m" # background green
-    INFO="\033[43m" # yellow text
-    BACKGROUND="\033[46m" # background blue
-    BLACKTEXT="\033[30m"
-    COLOR="\033[39m" # text white
+    ERROR="${BG_RED}${FG_WHITE}"
+    SUCCESS="${BG_GREEN}${FG_BLACK}"
+    INFO="${BG_YELLOW}${FG_BLACK}"
+    LABEL="${BG_BLUE}${FG_WHITE}"
     NC="\033[0m" # reset
 else
-    ERROR="-->"
-    GREEN="-->"
-    INFO="-->"
-    BACKGROUND="-->"
-    BLACKTEXT="-->"
-    COLOR="-->"
-    NC="-->"
+    ERROR=""
+    SUCCESS=""
+    INFO=""
+    LABEL=""
+    NC=""
 fi
 
 GIT=`which git`
@@ -52,7 +64,7 @@ function _install () {
         echo ". ${ALIASES_SCRIPT}" >> $ZSH_FILE
     fi
 
-    echo "${GREEN}"
+    echo "${FG_GREEN}"
     echo '-----Installed-----'
     echo ' '
     echo "!!!! Don't forget to run:"
