028a939078450176592789ce9f40831f2d7d4f8f
Bogdan Rizac <mariusbogdan83@gmail.com>
2017-08-20 12:54:27 +0200

    removed unused variables from install.sh script

--- install.sh
+++ install.sh
@@ -1,18 +1,13 @@
 #!/usr/bin/env bash
 
 # Foreground colors
-FG_RED="\033[31m"
 FG_WHITE="\033[97m"
 FG_GREEN="\033[32m"
 FG_BLACK="\033[30m"
-FG_YELLOW="\033[33m"
-FG_BLUE="\033[34m"
 
 # Background colors
 BG_RED="\033[41m"
-BG_WHITE="\033[107m"
 BG_GREEN="\033[42m"
-BG_BLACK="\033[40m"
 BG_YELLOW="\033[43m"
 BG_BLUE="\033[44m"
 
@@ -20,15 +15,11 @@ BG_BLUE="\033[44m"
 # supports them.
 if [ -t 1 ]; then
     ERROR="${BG_RED}${FG_WHITE}"
-    SUCCESS="${BG_GREEN}${FG_BLACK}"
     INFO="${BG_YELLOW}${FG_BLACK}"
-    LABEL="${BG_BLUE}${FG_WHITE}"
     NC="\033[0m" # reset
 else
     ERROR=""
-    SUCCESS=""
     INFO=""
-    LABEL=""
     NC=""
 fi
 
