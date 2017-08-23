46ce3f1badb71bf906eb213547bfff76421fa1ee
Bogdan Rizac <mariusbogdan83@gmail.com>
2017-08-23 15:03:25 +0200

    fixed allow_ssh_auth command

--- plugins/ssh.plugin.sh
+++ plugins/ssh.plugin.sh
@@ -2,5 +2,5 @@
 
 function allow_ssh_auth {
     dotfiles_log "Register SSH key to server: ${*}" "SSH"
-    $(which ssh-copy-id) -i "${HOME}/.ssh/id_rsa.pub" "${*}"
+    $(which ssh-copy-id) -i "${HOME}/.ssh/id_rsa" $*
 }
