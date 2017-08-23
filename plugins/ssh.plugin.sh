377d709c36d480d5191b68ff3a16b9f64f5dfdc7
Bogdan Rizac <mariusbogdan83@gmail.com>
2017-08-23 14:56:48 +0200

    add ssh copy id

--- plugins/ssh.plugin.sh
+++ plugins/ssh.plugin.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 function allow_ssh_auth {
-    dotfiles_log "Register SSH key to server: ${1}" "SSH"
-    $(which ssh-copy-id) -i "${HOME}/.ssh/id_rsa.pub" "${1}"
+    dotfiles_log "Register SSH key to server: ${*}" "SSH"
+    $(which ssh-copy-id) -i "${HOME}/.ssh/id_rsa.pub" "${*}"
 }
