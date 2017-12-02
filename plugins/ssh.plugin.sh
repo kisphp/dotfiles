a265aa50a56e84a99e4e9f2946276c7cffad77c6
kisphp <mariusbogdan83@gmail.com>
2017-12-02 14:53:57 +0100

    fixed allow_ssh_auth function

--- plugins/ssh.plugin.sh
+++ plugins/ssh.plugin.sh
@@ -1,7 +1,19 @@
 #!/usr/bin/env bash
 
 allow_ssh_auth () {
+    if [[ -z "$1" ]]; then
+        errorText "Please provide user@server_address"
+        echo "Usage:"
+        echo ""
+        echo "$0 user@server.com"
+        echo ""
+
+        return 1
+    fi
+
     dotfiles_log "Register SSH key to server: ${*}" "SSH"
     $(which ssh-copy-id) -i "${HOME}/.ssh/id_rsa" $*
     writeErrorMessage "Could not register SSH key on remote server ${*}"
+
+    return 0
 }
