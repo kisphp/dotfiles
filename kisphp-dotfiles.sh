5aa943d3d98810bdfdcf09bb51773b41a1f14e75
Marius Bogdan Rizac <kisphp@users.noreply.github.com>
2019-04-13 13:07:32 +0200

    Auto change repo url (#17)
    * setup self update
    
    * add git
    
    * change directories
    
    * cleanup changes

--- kisphp-dotfiles.sh
+++ kisphp-dotfiles.sh
@@ -3,13 +3,11 @@
 # Path to your oh-my-zsh installation.
 export DOTFILES=$HOME/.dotfiles
 
-# create config files in user's home directory if does not exists
-if [[ ! -f $HOME/.dotfiles.cfg ]]; then
-    cp "${DOTFILES}/.dotfiles.cfg.dist" "${HOME}/.dotfiles.cfg"
-fi
-
-# load config file
-. "${HOME}/.dotfiles.cfg"
+KP_DEBUG=0
+KP_LOGS=1
+KP_COLORS=1
+KP_UPGRADE_DAYS=120
+KP_UPGRADE_CONFIRMATION=1
 
 if [ "${KP_DEBUG}" -eq 1 ];then
     set -x
@@ -19,20 +17,14 @@ fi
 . "${DOTFILES}/tools/helpers.sh"
 . "${DOTFILES}/tools/logger.sh"
 . "${DOTFILES}/tools/print.sh"
-. "${DOTFILES}/tools/validations.sh"
 . "${DOTFILES}/tools/upgrade.sh"
 
-# load plugins
-for PLUGIN in $(find $DOTFILES/plugins -type f -name '*.plugin.sh'); do
-    . $PLUGIN
-done
-
-# load custom aliases
-if [[ ! -f ~/.dotfiles/custom.sh ]]; then
-    infoText "Write custom.sh file"
-    cp ~/.dotfiles/custom.sh.dist ~/.dotfiles/custom.sh
-fi
-. "${DOTFILES}/custom.sh"
+errorText "This project was moved to GITLAB."
+cd $DOTFILES
+git remote set-url origin https://gitlab.com/marius-rizac/dotfiles.git
+git reset --hard
+git pull --rebase -v
+cd -
 
 if [ "${KP_DEBUG}" -eq 1 ];then
     set +x
