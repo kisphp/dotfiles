2105ecf37ce728ef903099b187c15c73ba7d12b0
Bogdan Rizac <bogdan.rizac@lumas.com>
2019-04-13 13:07:00 +0200

    cleanup changes

--- Makefile
+++
@@ -1,7 +0,0 @@
-.PHONY: build run
-
-build:
-	docker build -t dotfiles-test .
-
-run:
-	docker run --rm -w /root -it dotfiles-test bash
--- kisphp-dotfiles.sh
+++ kisphp-dotfiles.sh
@@ -3,9 +3,6 @@
 # Path to your oh-my-zsh installation.
 export DOTFILES=$HOME/.dotfiles
 
-
-echo "-- hello world --"
-
 KP_DEBUG=0
 KP_LOGS=1
 KP_COLORS=1
@@ -20,7 +17,6 @@ fi
 . "${DOTFILES}/tools/helpers.sh"
 . "${DOTFILES}/tools/logger.sh"
 . "${DOTFILES}/tools/print.sh"
-#. "${DOTFILES}/tools/validations.sh"
 . "${DOTFILES}/tools/upgrade.sh"
 
 errorText "This project was moved to GITLAB."
@@ -29,18 +25,6 @@ git remote set-url origin https://gitlab.com/marius-rizac/dotfiles.git
 git reset --hard
 git pull --rebase -v
 cd -
-
-## load plugins
-#for PLUGIN in $(find $DOTFILES/plugins -type f -name '*.plugin.sh'); do
-#    . $PLUGIN
-#done
-#
-## load custom aliases
-#if [[ ! -f ~/.dotfiles/custom.sh ]]; then
-#    infoText "Write custom.sh file"
-#    cp ~/.dotfiles/custom.sh.dist ~/.dotfiles/custom.sh
-#fi
-#. "${DOTFILES}/custom.sh"
 
 if [ "${KP_DEBUG}" -eq 1 ];then
     set +x
